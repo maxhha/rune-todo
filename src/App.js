0a4fb6f464cccdef2b8b3ceb3e320bef98d846e6
maxhha <devmaxhha@gmail.com>
2019-07-14 14:23:45 +0300

    Add maket

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+function Note(props){
+    return (
+        <li className="note">
+            <div className="note-rune">
+                <span>{props.text[0]}</span>
+            </div>
+            <div className="note-content">
+                <div className="note-content__rune">
+                    {props.text}
+                </div>
+                <div className="note-content__text">
+                    {props.text}
+                </div>
+            </div>
+            <div className="note-button-wrapper">
+                <button></button>
+            </div>
+        </li>
+    );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <header>
+            <h1>RUNE TODO</h1>
+
+            <h2>rune todo</h2>
+        </header>
+        <div className="content">
+            <div className="content-inner">
+                <div className="content__new-note">
+                    <textarea
+                        placeholder="Write note here"
+                    />
+                <button>PUSH</button>
+                </div>
+                <hr />
+                <ul className="notes-list">
+                    <Note text="Random text"/>
+                    <Note text="Hello world!"/>
+                    <Note text="Another note"/>
+                </ul>
+                <div className="content-to-top-wrapper">
+                    <button>TO TOP</button>
+                </div>
+            </div>
+        </div>
+        <footer>made by <a href="#">maxhha</a></footer>
     </div>
   );
 }
